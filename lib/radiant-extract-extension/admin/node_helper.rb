68b2799056ab17101ca6cbebd028aaf924e07a01
jsntv200 <jsntv200@gmail.com>
2011-05-13 18:58:15 +1000

    trying to hide the add_child on children pages

--- lib/radiant-extract-extension/admin/node_helper.rb
+++ lib/radiant-extract-extension/admin/node_helper.rb
@@ -3,11 +3,10 @@ module RadiantExtractExtension
     module NodeHelper
       def self.included(base)
         base.class_eval do
-          alias_method_chain :expander, :extract
           alias_method_chain :expanded, :extract
+          alias_method_chain :expander, :extract
           alias_method_chain :children_class, :extract
-          alias_method_chain :child_link_for, :extract
-          alias_method_chain :child_menu_for, :extract
+          #alias_method_chain :children_for, :extract
         end
       end
 
@@ -18,7 +17,7 @@ module RadiantExtractExtension
 
       # Don't display the expander arrow
       def expander_with_extract(level)
-        @current_node.extracted? ? nil : expander_without_extract(level)
+        @current_node.extracted? ? '' : expander_without_extract(level)
       end
 
       # Force the no_children class so the SiteMapBehavior js will ignore an extract row
@@ -26,14 +25,8 @@ module RadiantExtractExtension
         @current_node.extracted? ? ' no_children' : children_class_without_extract
       end
 
-      # Remove ability to add children
-      def child_link_for_with_extract(page)
-        @current_node.parent.extracted? ? nil : child_link_for_without_extract(page)
-      end
-
-      # Remove ability to add children menu
-      def child_menu_for_with_extract(page)
-        @current_node.parent.extracted? ? nil : child_menu_for_without_extract(page)
+      def children_for_with_extract(page)
+        # page.parent.extracted? ? [] : children_for_without_extract(page)
       end
     end
   end
