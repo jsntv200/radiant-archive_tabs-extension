dbb8edb4049c838d6ed9befb15758bc956976418
jsntv200 <jsntv200@gmail.com>
2011-05-14 10:31:40 +1000

    cleanup and children_for spec

--- spec/helpers/admin/node_helper_spec.rb
+++ spec/helpers/admin/node_helper_spec.rb
@@ -2,18 +2,29 @@ require File.dirname(__FILE__) + "/../../spec_helper"
 
 describe Admin::NodeHelper do
   before :each do
-    assigns[:current_node] = mock_model(ExtractPage)
+    @extract = mock_model(ExtractPage)
+    @extract.stub!(:extracted?).and_return(true)
   end
 
   it "should not expand the current node" do
+    assigns[:current_node] = @extract
     helper.expanded.should be_false
   end
 
   it "should have a no_children class for the current node" do
+    assigns[:current_node] = @extract
     helper.children_class.should == " no_children"
   end
 
   it "should not render an arrow for the current node" do
+    assigns[:current_node] = @extract
     helper.expander(0).should == ""
+  end
+
+  it "should return no children if the parent is an ExtractPage" do
+    @page = mock_model(Page)
+    @page.stub!(:parent).and_return(@extract)
+    assigns[:current_node] = @page
+    helper.children_for(@page).should == []
   end
 end
