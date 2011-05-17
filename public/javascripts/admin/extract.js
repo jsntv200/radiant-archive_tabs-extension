36599398aab0a3e7c71f7a54a34b970def72d07d
jsntv200 <jsntv200@gmail.com>
2011-05-17 19:22:28 +1000

    moved the js inline

--- public/javascripts/admin/extract.js
+++
@@ -1,9 +0,0 @@
-document.observe("dom:loaded", function() {
-  var page = location.pathname.match('/admin/pages/[0-9]+/edit'),
-      link = $$('#edit_page .buttons a').first()
-
-  if (page && link) {
-    var parent_id = $('page_parent_id').readAttribute('value')
-    link.writeAttribute('href', link.readAttribute('href') + '/' + parent_id + '/children')
-  }
-});
